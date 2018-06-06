17d6011f35cee14c2bc4a06634766d564e39ff65
vinays <Vinay_Singh2@mentor.com>
2018-06-06 18:36:07 +0530

    Removing files

--- bash_settings/bash_software_source/a942.sh
+++
@@ -1,4 +0,0 @@
- ###### Acrobat reader 9.4.2 #######
-
-pa=`echo ${PATH} | sed -e 's/\/in\/Acrobat942\/bin//g'`
-export PATH=/in/Acrobat942/bin:$pa
--- bash_settings/bash_software_source/tkdiff-4.2.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- pack=/in/tcl/tkdiff/4.2
- if  [[  `uname` == Linux  ]]  ; then
-     export PATH=$pack:$PATH
- else
-     echo "TKDIFF 4.2 can be sourced for Linux machines"
-     exit 0
- fi
